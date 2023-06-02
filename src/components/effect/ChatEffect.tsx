d26d3563850f12a89c3025e0ea04690f45ac472a
bobby169 <115393304@qq.com>
2023-06-02 18:47:37 +0800

    useEffect

--- src/components/effect/ChatEffect.tsx
+++ src/components/effect/ChatEffect.tsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 import { createConnection } from "./Chat";
 import { useChatRoom } from "./UseChatRoom";
+import Counter from "./Counter";
 
 function ChatRoom({ roomId }) {
   const [serverUrl, setServerUrl] = useState("https://localhost:1234");
-  useChatRoom({ serverUrl, roomId });
+  const [message, setMessage] = useState("");
+  // useChatRoom({ serverUrl, roomId });
 
-  // useEffect(() => {
-  //   const connection = createConnection(serverUrl, roomId);
-  //   connection.connect();
-  //   return () => {
-  //     connection.disconnect();
-  //   };
-  // }, [roomId, serverUrl]);
+  useEffect(() => {
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect();
+    return () => {
+      connection.disconnect();
+    };
+    // 如果您根本不传递任何依赖项数组(不传第二个参数)，您的 Effect 将在您的组件的每次渲染（和重新渲染）之后运行。
+  }, [roomId, serverUrl]); // 1. 如果这里没有依赖项，输入message渲染都会创建一个新的连接 2. 如果是空数组，只会创建一个连接，但是serverUrl和roomId不会更新
 
   return (
     <>
@@ -24,6 +27,10 @@ function ChatRoom({ roomId }) {
         />
       </label>
       <h1>Welcome to the {roomId} room!</h1>
+      <label>
+        Your message:{" "}
+        <input value={message} onChange={(e) => setMessage(e.target.value)} />
+      </label>
     </>
   );
 }
@@ -33,6 +40,7 @@ export default function ChatEffect() {
   const [show, setShow] = useState(false);
   return (
     <>
+      <Counter />
       <label>
         Choose the chat room:{" "}
         <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
---
+++ src/components/effect/Counter.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // setCount(count + 1); // ❌ This effect depends on the `count` state
+      setCount((c) => c + 1); // ✅ Pass a state updater
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []); // ✅ Now count is not a dependency
+
+  return <h1>{count}</h1>;
+}
